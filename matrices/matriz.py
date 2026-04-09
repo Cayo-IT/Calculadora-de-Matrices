22f194a985d2711a0ff5c523f3085b7b60bb1e87
Carlos Omar Sanchez Torrescano <carlosbusinessworking@hotmail.com>
2026-04-09 09:46:02 -0700

    Agregar primera version clase Matriz

---
+++ matrices/matriz.py
@@ -0,0 +1,23 @@
+class Matriz:
+    def __init__(self, values):
+        size 
+        #for i in range(0, len(values)):
+        cols = None
+        for row in values:
+            if cols == None:
+                cols = len(row)
+            elif cols != len(row):
+                    raise ValueError("La matriz debe de tener las mismas columnas en cada fila")
+            
+        if cols == 0:
+             raise ValueError("Las filas deben tener al menos una columna..")
+        self.values = values
+        self.columns = cols
+        self.rows = len(values)
+
+    def __getitem__(self, index):
+         if index < 0 or index >= self.rows:
+              raise IndexError(f"Indice para fila fuera de rango! ({index})")
+         return self.values[index]
+    
+    
