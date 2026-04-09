d5db9a0d6172577fac2bf044bf3516eecd984a3a
Carlos Omar Sanchez Torrescano <carlosbusinessworking@hotmail.com>
2026-04-09 08:45:57 -0700

    Inicio del proyecto

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Calculadora de Matrices
+-Entrada de matrices: Por archivo, por consola 
+-Operaciones:
+Suma, Resta,  Multiplicacion por escalar, Multiplicacion de Matrices,
+Matriz inversa, Transpuesta
+-Solucion de sistema de ecuaciones lineales con matriz inversa
+-Validacion de entrada del usuario:
+Verificar entrada, Dar mensajes de error, Repetir la solicitud de entrada
+-Guardar resultados en un archivo 
+Modulos:
+-Validacion de una matriz
+-Entrada y salida de matrices en archivos
+-Operaciones de matrices
+-Main
+-Interfaz de usuario 
+'''
+#librerias
+import numpy as np
