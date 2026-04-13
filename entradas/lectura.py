8355318c09f6473982cf73a77937a172f072afad
Carlos Omar Sanchez Torrescano <carlosbusinessworking@hotmail.com>
2026-04-13 08:53:35 -0700

    Correccion de codigo

--- entradas/lectura.py
+++ entradas/lectura.py
@@ -1,10 +1,13 @@
 import os
 from pathlib import Path 
 
-def read_file(ruta):
-    archivo = Path(os.getcwd() / ".." / "resources" / matriz.txt)
-    print(archivo.resolve())
+def read_file():
+    archivo1 = Path(__file__).parent / ".." #Muestra la direccion exacta / ".." / "resources" / "matrices" / "matriz.txt"
+    archivo2 = Path(os.getcwd()) #Esta es la carpeta en donde estoy trabajando / ".." / "resources" / "matrices"/ "matriz.txt"
+    print("getcwd():", archivo1.resolve())
+    print("__file__:", archivo2.resolve())
 
-
-    if __name__ == "__main__":
-        read_file()
+    #with open(archivo1.resolve(), "r") as readfile:
+        #print(readfile.readlines())
+if __name__ == "__main__":
+    read_file()
